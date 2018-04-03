cb09f500efc8d63b195c0141488ff018ccb4e2a6
sunbo <sunbo5439@qq.com>
2018-04-03 19:06:53 +0800

    i

--- Speech_Recgnition/driver.py
+++ Speech_Recgnition/driver.py
@@ -75,7 +75,10 @@ def _infer(model, wav_file_path, num_word_list, hps):
     wav, sr = librosa.load(wav_file_path, mono=True)
     mfcc = np.transpose(np.expand_dims(librosa.feature.mfcc(wav, sr), axis=0), [0, 2, 1])
     pad_len = hps.wav_max_len - (mfcc.shape[1])
-    mfcc = np.concatenate((mfcc, np.zeros((hps.batch_size, pad_len, hps.n_mfcc), dtype=int)), axis=1)
+    if pad_len > 0:
+        mfcc = np.concatenate((mfcc, np.zeros((hps.batch_size, pad_len, hps.n_mfcc), dtype=int)), axis=1)
+    else:
+        mfcc = mfcc[:hps.wav_max_len:]
     saver = tf.train.Saver()
     sv = tf.train.Supervisor(logdir=FLAGS.log_root,
                              is_chief=True,
--- Speech_Recgnition/driver2.py
+++ Speech_Recgnition/driver2.py
@@ -8,6 +8,7 @@ import data_preprocess
 import codecs
 import os
 import Levenshtein
+
 """
 train:  python driver.py 
 infer:  python driver.py --mode=infer --wav_file_path=D8_999.wav
@@ -50,35 +51,39 @@ def _infer_batch(model, wav_file_paths, num_word_list, hps):
                              global_step=model.global_step)
     sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(allow_soft_placement=True))
     to_word = lambda num: num_word_list[num]
-    generated_summay_list=[]
+    generated_summay_list = []
     for wav_file_path in wav_file_paths:
         print(wav_file_path)
         wav, sr = librosa.load(wav_file_path, mono=True)
         mfcc = np.transpose(np.expand_dims(librosa.feature.mfcc(wav, sr), axis=0), [0, 2, 1])
         pad_len = hps.wav_max_len - (mfcc.shape[1])
-        mfcc = np.concatenate((mfcc, np.zeros((hps.batch_size, pad_len, hps.n_mfcc), dtype=int)), axis=1)
+        if pad_len > 0:
+            mfcc = np.concatenate((mfcc, np.zeros((hps.batch_size, pad_len, hps.n_mfcc), dtype=int)), axis=1)
+        else:
+            mfcc = mfcc[:hps.wav_max_len:]
         predict, global_step, seq_len = model.run_infer(sess=sess, mfcc=mfcc)
         predict_word = [''.join(list(map(to_word, pd))) for pd in predict]
-        generated_summay =''
+        generated_summay = ''
         for sentence in predict_word:
             generated_summay += sentence.replace(' ', '')
-        with codecs.open(wav_file_path+'.trngen','w',encoding='utf-8') as f:
+        with codecs.open(wav_file_path + '.trngen', 'w', encoding='utf-8') as f:
             f.write(generated_summay)
         generated_summay_list.append(generated_summay)
 
-def eval(label_path_list,gen_path_list):
-    assert  len(label_path_list)==len(gen_path_list)
-    total_distance,total_len=0,0
+
+def eval(label_path_list, gen_path_list):
+    assert len(label_path_list) == len(gen_path_list)
+    total_distance, total_len = 0, 0
     for i in range(len(label_path_list)):
-        lf=codecs.open(label_path_list[i],'r','utf-8')
-        gf=codecs.open(gen_path_list[i],'r','utf-8')
-        s1=lf.readline()
-        s2=gf.readline()
+        lf = codecs.open(label_path_list[i], 'r', 'utf-8')
+        gf = codecs.open(gen_path_list[i], 'r', 'utf-8')
+        s1 = lf.readline()
+        s2 = gf.readline()
         lf.close()
         gf.close()
-        total_distance+=Levenshtein.distance(s1,s2)
-        total_len+=len(s1)
-    print("CER:%f"%(total_distance*1.8/total_len))
+        total_distance += Levenshtein.distance(s1, s2)
+        total_len += len(s1)
+    print("CER:%f" % (total_distance * 1.8 / total_len))
 
 
 def main(unused_argv):
@@ -99,17 +104,15 @@ def main(unused_argv):
 
     test_folder = 'data_thchs30/test'
     data_folder = 'data_thchs30/data'
-    wav_file_list,label_path_list,gen_path_list=[],[],[]
+    wav_file_list, label_path_list, gen_path_list = [], [], []
     for e in os.listdir(test_folder):
         if e.endswith('wav'):
-            wav_file_list.append(os.path.join(test_folder,e))
-            label_path_list.append(os.path.join(data_folder,e+'.trn'))
-            gen_path_list.append(os.path.join(test_folder,e+'.trngen'))
+            wav_file_list.append(os.path.join(test_folder, e))
+            label_path_list.append(os.path.join(data_folder, e + '.trn'))
+            gen_path_list.append(os.path.join(test_folder, e + '.trngen'))
     infer_hps = hps._replace(batch_size=1)
     model = neural_model.Model(hps=infer_hps)
     _infer_batch(model, wav_file_list, num_word_list, infer_hps)
-
-
 
 
 if __name__ == '__main__':
