737765446927e4ed9c85bcefc017b06901078b82
sunbo <sunbo5439@qq.com>
2018-04-03 21:59:28 +0800

    i

--- Speech_Recgnition/driver.py
+++ Speech_Recgnition/driver.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import librosa
 import data_preprocess
+import codecs,json
 
 """
 train:  python driver.py
@@ -20,6 +21,7 @@ tf.app.flags.DEFINE_string('labels_vec_path', 'model/labels_id.json',
 tf.app.flags.DEFINE_string('mode', 'train', 'train , infer')
 tf.app.flags.DEFINE_string('vocab_path', 'model/vocab.txt', 'vocab path')
 tf.app.flags.DEFINE_string('wav_file_path', 'D8_999.wav', 'inpuf file, needed when infer')
+tf.app.flags.DEFINE_string('wav_test_paths', 'model/wav_test.json', '')
 tf.app.flags.DEFINE_integer('batch_size', 16, 'batch size')
 tf.app.flags.DEFINE_float('lr', 0.001, 'learning rate')
 tf.app.flags.DEFINE_float('min_lr', 0.0005, 'min learning rate')
@@ -96,6 +98,33 @@ def _infer(model, wav_file_path, num_word_list, hps):
     for sentence in predict_word:
         print sentence.replace(' ', '_')
 
+def _infer_batch(model, wav_file_paths, num_word_list,result_path, hps):
+    model.build_model()
+    saver = tf.train.Saver()
+    sv = tf.train.Supervisor(logdir=FLAGS.log_root,
+                             is_chief=True,
+                             saver=saver,
+                             summary_op=None,
+                             global_step=model.global_step)
+    sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(allow_soft_placement=True))
+    to_word = lambda num: num_word_list[num]
+    generated_summay_list = []
+    for wav_file_path in wav_file_paths:
+        print(wav_file_path)
+        wav, sr = librosa.load(wav_file_path, mono=True)
+        mfcc = np.transpose(np.expand_dims(librosa.feature.mfcc(wav, sr), axis=0), [0, 2, 1])
+        pad_len = hps.wav_max_len - (mfcc.shape[1])
+        if pad_len > 0:
+            mfcc = np.concatenate((mfcc, np.zeros((hps.batch_size, pad_len, hps.n_mfcc), dtype=int)), axis=1)
+        else:
+            mfcc = mfcc[:, 0:hps.wav_max_len, :]
+        predict, global_step, seq_len = model.run_infer(sess=sess, mfcc=mfcc)
+        predict_word = [''.join(list(map(to_word, pd))) for pd in predict]
+        generated_summay = ''
+        for sentence in predict_word:
+            generated_summay += sentence.replace(' ', '')
+        generated_summay_list.append(generated_summay)
+        json.dump(generated_summay_list,codecs.open(result_path,'w',encoding='utf-8'),ensure_ascii=False,indent=4)
 
 def main(unused_argv):
     # get wav_max_len first
@@ -113,14 +142,19 @@ def main(unused_argv):
         lr=FLAGS.lr,
     )
 
-    if hps.mode == 'train':
+    if FLAGS.mode == 'train':
         model = neural_model.Model(hps=hps)
         batcher = neural_model.Batcher(hps)
         _train(model, batcher)
-    elif hps.mode == 'infer':
+    elif FLAGS.mode == 'infer':
         infer_hps = hps._replace(batch_size=1)
         model = neural_model.Model(hps=infer_hps)
         _infer(model, FLAGS.wav_file_path, num_word_list, infer_hps)
+    elif FLAGS.mode == 'batch_infer':
+        infer_hps = hps._replace(batch_size=1)
+        model = neural_model.Model(hps=infer_hps)
+        wav_file_paths = json.load(codecs.open(FLAGS.wav_test_paths,'r','utf-8'))
+        _infer_batch(wav_file_paths,num_word_list,'model/my_test_label.json',infer_hps)
 
 
 if __name__ == '__main__':
